a139acbb092bd6220ccca7a41164c7059b067532
mehrad <mehrad.kavian@gmail.com>
2012-08-28 01:57:58 +0430

    a lot of changes and finally its like iphone folders during edit

--- an idea for index/script.js
+++ an idea for index/script.js
@@ -1,12 +1,32 @@
+Math.rnd = Math.random;
+Math.random = function (a,b){
+	return Math.floor(Math.rnd()*(b-a+1))+a;
+};
+
 $(function () {
+	var vibrate = function(){
+		toggleContent.each(function(){
+			$(this).animate({
+				rotate:Math.random(-9,9)+'deg',
+				left:Math.random(-3,3),
+				top:Math.random(-3,3)
+			})
+		})
+	};
 	var options   = $('ul.options > li'),
 	toggleTargets = $('div.container ul.toggle');
+	toggleContent = $('div.container ul.toggle > li')
 	options.each(function(){
 		var that=this;
 		$(this).click(function(){
+			$(that).animate({
+				'box-shadow' : '0 0 10px #111'
+			},100)
 			toggleTargets.eq($(that).index()).animate({
 				height:300
-			},100,toggleTargets.eq($(that).index()).css({display:'block'}))
+			},170,toggleTargets.eq($(that).index()).css({display:'block'}));
+			
 		});
 	});
+	setInterval(vibrate,100)
 });
