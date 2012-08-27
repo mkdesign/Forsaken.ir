e594317f7a7516923645637a12f235954f725521
mehrad <mehrad.kavian@gmail.com>
2012-08-27 21:55:56 +0430

    some changes

--- an idea for index/script.js
+++ an idea for index/script.js
@@ -1,5 +1,14 @@
 $(function () {
 	var options   = $('ul.options > li'),
-	toggleTargets = $('div.container ul.toggle')
-	
+	toggleTargets = $('div.container ul.toggle');
+	options.each(function(){
+		$(this).click(function(){
+			toggleTargets.animte({
+				if(height<=0)
+					height:100
+				else
+					height:0
+			});
+		});
+	});
 });
