61495efa134f0a2b79679eb35578fc9b9ea07bb7
mehrad <mehrad.kavian@gmail.com>
2012-08-28 00:08:53 +0430

    starting to test

--- an idea for index/script.js
+++ an idea for index/script.js
@@ -2,13 +2,7 @@ $(function () {
 	var options   = $('ul.options > li'),
 	toggleTargets = $('div.container ul.toggle');
 	options.each(function(){
-		$(this).click(function(){
-			toggleTargets.animte({
-				if(height<=0)
-					height:100
-				else
-					height:0
-			});
+		this.click(function(){
+			alert(index);
 		});
 	});
-});
