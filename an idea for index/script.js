49efc0027986f66b8c300cf3baee02833809a448
mehrad <mehrad.kavian@gmail.com>
2012-08-31 00:29:53 +0430

    lots of changes

--- an idea for index/script.js
+++ an idea for index/script.js
@@ -1,39 +1,62 @@
-Math.rnd = Math.random;
+Math.rnd    = Math.random;
 Math.random = function (a,b){
+
 	return Math.floor(Math.rnd()*(b-a+1))+a;
 };
 
 $(function () {
+
 	var vibrate = function(){
+
 		toggleContent.each(function(){
+
 			$(this).animate({
-				rotate:Math.random(-25,25)+'deg',
-				left:Math.random(-8,8)
+
+				rotate : Math.random(-25,25)+'deg',
+				left   : Math.random(-8,8)
 				
-			})
-		})
+			});
+		});
 	};
-	var options   = $('ul.options > li'),
-	toggleTargets = $('div.container ul.toggle');
-	toggleContent = $('div.container ul.toggle > li')
+
+	var options       = $('ul.options > li'),
+		toggleTargets = $('div.container ul.toggle'),
+		toggleContent = $('div.container ul.toggle > li');
+
 	options.each(function(){
-		var that=this;
+
+		var that = this;
+
 		$(this).click(function(){
-			if(toggleTargets.css('display')=='none'){
+
+			if(toggleTargets.css('display') == 'none'){
+
 				var VibStart=setInterval(vibrate,55);
+
 				$(that).animate({
+
 					'box-shadow' : '0 0 10px #111'
+
 				},100);
+
 				toggleTargets.eq($(that).index()).animate({
-					height:300
+
+					height : 300
+
 				},170,toggleTargets.eq($(that).index()).css({display:'block'}));
 			}
 			else{
+
 				$(that).animate({
+
 					'box-shadow' : 'none'
+
 				},100);
+
 				toggleTargets.eq($(that).index()).animate({
-					height:0
+
+					height : 0
+
 				},170,toggleTargets.eq($(that).index()).css({display:'none'}));
 			}
 			
